e7156014f7e7bc1da296f64db4364d3a16a83bd4
tomoki <tmdesign.0223@gmail.com>
2023-12-21 22:06:12 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2023_12_19_062602_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('site');
+            $table->foreignId('user_id')->constrained();
+            $table->text('image_path')->nullable();
+            $table->text('body');
+            $table->timestamp('working_day');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
