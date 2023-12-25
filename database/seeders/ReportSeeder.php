907cf65e1098915e36bf5fd7a196725c6dcc0a47
tomoki <tmdesign.0223@gmail.com>
2023-12-25 22:40:26 +0900

    ReportSeeder変更

--- database/seeders/ReportSeeder.php
+++ database/seeders/ReportSeeder.php
@@ -2,11 +2,9 @@
 
 namespace Database\Seeders;
 
-use Carbon\Carbon;
+use App\Models\Report;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use Illuminate\Support\Facades\DB;
 
 class ReportSeeder extends Seeder
 {
@@ -15,40 +13,6 @@ class ReportSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('reports')->insert([
-            'site_name' => '歩道橋',
-            'user_id' => User::factory()->create()->id,
-            'image_path' => '',
-            'body' => '今日は剥離剤の塗布です',
-            'working_day' => Carbon::now()->format('Y-m-d'),
-            'start_time' => Carbon::now()->format('Y-m-d H:i:s'),
-            'end_time' => Carbon::now()->format('Y-m-d H:i:s'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-
-        DB::table('reports')->insert([
-            'site_name' => '無名橋',
-            'user_id' => User::factory()->create()->id,
-            'image_path' => '',
-            'body' => '今日はケレンです',
-            'working_day' => Carbon::now()->format('Y-m-d'),
-            'start_time' => Carbon::now()->format('Y-m-d H:i:s'),
-            'end_time' => Carbon::now()->format('Y-m-d H:i:s'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-
-        DB::table('reports')->insert([
-            'site_name' => '新築',
-            'user_id' => User::factory()->create()->id,
-            'image_path' => '',
-            'body' => '今日は新築の内装です',
-            'working_day' => Carbon::now()->format('Y-m-d'),
-            'start_time' => Carbon::now()->format('Y-m-d H:i:s'),
-            'end_time' => Carbon::now()->format('Y-m-d H:i:s'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+        Report::factory(5)->create();
     }
 }
