a5a001306427c80a1de2ffec8da7faa8951e88ec
tomoki <tmdesign.0223@gmail.com>
2023-12-25 22:47:34 +0900

    reportのルートをmiddlewareの中に移動

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/report', [ReportController::class, 'index'])->name('report.index');
 });
 
 require __DIR__ . '/auth.php';
