9215a810969b39421b574a998a168cc5599e5021
tomoki <tmdesign.0223@gmail.com>
2023-12-26 22:19:46 +0900

    reportsデータの表示

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -11,24 +11,17 @@
 
     <body>
         <div class="bg-gray-300 p-8">
-            <h1 class="text-2xl font-bold text-center mb-4">業務日報一覧</h1>
-            <div class="bg-white p-4 mb-4">
-                <div class="flex justify-between items-center">
-                    <h2 class="text-xl font-bold"><a href="#">刈羽歩道橋</a></h2>
-                    <p class="text-gray-500">2023-12-23</p>
-                </div>
-            </div>
-            <div class="bg-white p-4 mb-4">
-                <div class="flex justify-between items-center">
-                    <h2 class="text-xl font-bold"><a href="#">亀田製菓</a></h2>
-                    <p class="text-gray-500">2023-12-21</p>
-                </div>
-            </div>
-            <div class="bg-white p-4 mb-4">
-                <div class="flex justify-between items-center">
-                    <h2 class="text-xl font-bold"><a href="#">度合歩道橋</a></h2>
-                    <p class="text-gray-500">2023-12-24</p>
-                </div>
+            <div class="w-3/5 m-auto mt-8">
+                <h1 class="text-2xl font-bold text-center mb-8">業務日報一覧</h1>
+                @foreach ($reports as $report)
+                    <div class="bg-white p-8 mb-4 rounded-md shadow-md">
+                        <div class="flex justify-between items-center">
+                            <h2 class="text-xl font-bold"><a href="#">{{ $report->site_name }}</a></h2>
+                            <p class="text-gray-500">作業日:{{ $report->working_day }}</p>
+                        </div>
+                    </div>
+                @endforeach
+                {{ $reports->links() }}
             </div>
         </div>
     </body>
