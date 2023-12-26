74a7e9bb5dc5c4d9153eb6878ed509f634c5fe7e
tomoki <tmdesign.0223@gmail.com>
2023-12-26 22:19:29 +0900

    reportsテーブルのデータの取得

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Report;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function index()
     {
-        return  view('report.index');
+        $reports = Report::orderBy('working_day', 'desc')->paginate(10);
+        return  view('report.index', compact('reports'));
     }
 }
