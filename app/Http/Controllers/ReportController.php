db6ae39ad39304b0155befef3eb8fc37374e5fb9
tomoki <tmdesign.0223@gmail.com>
2023-12-27 23:40:22 +0900

    withメソッド追加

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,7 +9,7 @@ class ReportController extends Controller
 {
     public function index()
     {
-        $reports = Report::orderBy('working_day', 'desc')->paginate(10);
+        $reports = Report::orderBy('working_day', 'desc')->with('user')->paginate(10);
         return  view('report.index', compact('reports'));
     }
 }
