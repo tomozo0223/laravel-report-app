ebc4390abf336d3c1982e9b42ea38f806109f146
tomoki <tmdesign.0223@gmail.com>
2023-12-30 22:44:31 +0900

    詳細画面のルート追加

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/report', [ReportController::class, 'index'])->name('report.index');
+    Route::get('/report/{report}', [ReportController::class, 'show'])->name('report.show');
 });
 
 require __DIR__ . '/auth.php';
