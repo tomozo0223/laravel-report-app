65c15b351666643fb06d8117b50f4cac3289c490
tomoki <tmdesign.0223@gmail.com>
2024-01-17 22:12:24 +0900

    ユーザー一覧ページ

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <!DOCTYPE html>
+    <html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>ユーザー一覧ページ</title>
+    </head>
+
+    <body>
+        <div class="p-8">
+            <div class="w-3/5 m-auto mt-8">
+                <h1 class="text-2xl font-bold text-center mb-8">ユーザー一覧</h1>
+                <div class="overflow-x-auto mb-4">
+                    <table class="min-w-full border border-gray-600">
+                        <thead>
+                            <tr>
+                                <th class="py-2 px-4 bg-gray-200 border-b">ID</th>
+                                <th class="py-2 px-4 bg-gray-200 border-b">名前</th>
+                                <th class="py-2 px-4 bg-gray-200 border-b">メールアドレス</th>
+                                <th class="py-2 px-4 bg-gray-200 border-b">登録日</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr class="hover:bg-gray-100">
+                                    <td class="py-2 px-4 border-b">{{ $user->id }}</td>
+                                    <td class="py-2 px-4 border-b"><a href="#">{{ $user->name }}</a></td>
+                                    <td class="py-2 px-4 border-b">{{ $user->email }}</td>
+                                    <td class="py-2 px-4 border-b">{{ $user->created_at }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                {{ $users->links() }}
+            </div>
+        </div>
+    </body>
+
+    </html>
+</x-app-layout>
