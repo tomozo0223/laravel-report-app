3774cdbdbf32aad45635c7ee0f5f5ba2f4b2eabc
tomoki <tmdesign.0223@gmail.com>
2024-01-17 22:30:49 +0900

    ナビゲーションメニュー追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,15 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('report.index')" :active="request()->routeIs('report.index')">
+                        日報一覧
                     </x-nav-link>
+                    <x-nav-link :href="route('report.create')" :active="request()->routeIs('report.create')">
+                        日報登録
+                    </x-nav-link>
+                    {{-- <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
+                        ユーザー一覧
+                    </x-nav-link> --}}
                 </div>
             </div>
 
@@ -75,9 +81,15 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('report.index')" :active="request()->routeIs('report.index')">
+                日報一覧
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('report.create')" :active="request()->routeIs('report.create')">
+                日報登録
+            </x-responsive-nav-link>
+            {{-- <x-responsive-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
+                ユーザー一覧
+            </x-responsive-nav-link> --}}
         </div>
 
         <!-- Responsive Settings Options -->
