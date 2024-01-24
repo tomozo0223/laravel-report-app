37992665e5d43f546582edc26e4dd1b41e697250
tomoki <tmdesign.0223@gmail.com>
2024-01-24 23:28:27 +0900

    コンポーネント呼び出し

--- resources/views/report/edit.blade.php
+++ resources/views/report/edit.blade.php
@@ -10,67 +10,78 @@
     </head>
 
     <body>
-        <x-reports.layout title="業務日報編集">
-            <form action="{{ route('report.update', $report) }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                <div class="mb-4">
-                    <label for="site_name" class="block text-sm font-semibold text-gray-600">現場名:</label>
-                    <x-reports.error name='site_name'>
-                    </x-reports.error>
-                    <input type="text" name="site_name" id="site_name" class="w-full p-2 border rounded-md"
-                        value="{{ old('site_name', $report->site_name) }}">
+        <div class="bg-gray-300 p-8">
+            <div class="w-4/5 m-auto mt-8">
+                <h1 class="text-2xl font-bold text-center mb-8">業務日報登録</h1>
+                <div class="max-w-2xl mx-auto p-8 bg-white border rounded-md shadow-md">
+                    <form action="{{ route('report.update', $report) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-4">
+                            <label for="site_name" class="block text-sm font-semibold text-gray-600">現場名:</label>
+                            @error('site_name')
+                                <p class="text-red-600">{{ $message }}</p>
+                            @enderror
+                            <input type="text" name="site_name" id="site_name" class="w-full p-2 border rounded-md"
+                                value="{{ old('site_name', $report->site_name) }}">
+                        </div>
+                        <div class="mb-4">
+                            <label for="working_day" class="block text-sm font-semibold text-gray-600">作業日:</label>
+                            @error('working_day')
+                                <p class="text-red-600">{{ $message }}</p>
+                            @enderror
+                            <input type="date" name="working_day"
+                                id="working_day"class="w-full p-2 border rounded-md"
+                                value="{{ old('working_day', $report->working_day) }}">
+                        </div>
+                        <div class="mb-4">
+                            <label for="start_time" class="block text-sm font-semibold text-gray-600">開始時間:</label>
+                            @error('start_time')
+                                <p class="text-red-600">{{ $message }}</p>
+                            @enderror
+                            <input type="time" name="start_time" id="start_time" class="w-full p-2 border rounded-md"
+                                value="{{ old('start_time', $report->start_time) }}">
+                        </div>
+                        <div class="mb-4">
+                            <label for="end_time" class="block text-sm font-semibold text-gray-600">終了時間:</label>
+                            @error('end_time')
+                                <p class="text-red-600">{{ $message }}</p>
+                            @enderror
+                            <input type="time" name="end_time" id="end_time" class="w-full p-2 border rounded-md"
+                                value="{{ old('end_time', $report->end_time) }}">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-sm font-semibold text-gray-600">作業メンバー:</label>
+                            @foreach ($users as $user)
+                                <label class="inline-flex items-center">
+                                    <input type="checkbox" name="user_id[]" value="{{ $user->id }}" class="mr-2"
+                                        @checked(in_array($user->id, old('user_id', $reportUserId)))>
+                                    {{ $user->name }} </label>
+                            @endforeach
+                        </div>
+                        <div class="mt-4">
+                            <label for="image" class="block text-sm font-medium text-gray-700">
+                                画像を選択
+                            </label>
+                            <input type="file" name="image" id="image">
+                        </div>
+                        <div class="mb-4 mt-4">
+                            <label for="body" class="block text-sm font-semibold text-gray-600">作業内容:</label>
+                            @error('body')
+                                <p class="text-red-600">{{ $message }}</p>
+                            @enderror
+                            <textarea name="body" id="body" cols="30" rows="5" class="w-full p-2 border rounded-md"
+                                placeholder="作業内容を記入してください">{{ old('body', $report->body) }}</textarea>
+                        </div>
+                        <div class="text-right">
+                            <x-primary-button>
+                                登録
+                            </x-primary-button>
+                        </div>
+                    </form>
                 </div>
-                <div class="mb-4">
-                    <label for="working_day" class="block text-sm font-semibold text-gray-600">作業日:</label>
-                    <x-reports.error name='working_day'>
-                    </x-reports.error>
-                    <input type="date" name="working_day" id="working_day"class="w-full p-2 border rounded-md"
-                        value="{{ old('working_day', $report->working_day) }}">
-                </div>
-                <div class="mb-4">
-                    <label for="start_time" class="block text-sm font-semibold text-gray-600">開始時間:</label>
-                    <x-reports.error name='start_time'>
-                    </x-reports.error>
-                    <input type="time" name="start_time" id="start_time" class="w-full p-2 border rounded-md"
-                        value="{{ old('start_time', $report->start_time) }}">
-                </div>
-                <div class="mb-4">
-                    <label for="end_time" class="block text-sm font-semibold text-gray-600">終了時間:</label>
-                    <x-reports.error name='end_time'>
-                    </x-reports.error>
-                    <input type="time" name="end_time" id="end_time" class="w-full p-2 border rounded-md"
-                        value="{{ old('end_time', $report->end_time) }}">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-sm font-semibold text-gray-600">作業メンバー:</label>
-                    @foreach ($users as $user)
-                        <label class="inline-flex items-center">
-                            <input type="checkbox" name="user_id[]" value="{{ $user->id }}" class="mr-2"
-                                @checked(in_array($user->id, old('user_id', $reportUserId)))>
-                            {{ $user->name }} </label>
-                    @endforeach
-                </div>
-                <div class="mt-4">
-                    <label for="image" class="block text-sm font-medium text-gray-700">
-                        画像を選択
-                    </label>
-                    <input type="file" name="image" id="image">
-                </div>
-                <div class="mb-4 mt-4">
-                    <label for="body" class="block text-sm font-semibold text-gray-600">作業内容:</label>
-                    <x-reports.error name='body'>
-                    </x-reports.error>
-                    <textarea name="body" id="body" cols="30" rows="5" class="w-full p-2 border rounded-md"
-                        placeholder="作業内容を記入してください">{{ old('body', $report->body) }}</textarea>
-                </div>
-                <div class="text-right">
-                    <x-primary-button>
-                        登録
-                    </x-primary-button>
-                </div>
-            </form>
-        </x-reports.layout>
+            </div>
+        </div>
     </body>
 
     </html>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,36 +10,41 @@
     </head>
 
     <body>
-        <x-reports.layout title="ユーザー一覧">
-            <table class="w-4/5 m-auto border border-gray-600">
-                <thead>
-                    <tr>
-                        <th class="py-2 px-4 bg-gray-200 border-b text-left">ID</th>
-                        <th class="py-2 px-4 bg-gray-200 border-b text-left">名前</th>
-                        <th class="py-2 px-4 bg-gray-200 border-b text-left">メールアドレス</th>
-                        <th class="py-2 px-4 bg-gray-200 border-b text-left">雇用状態</th>
-                        <th class="py-2 px-4 bg-gray-200 border-b text-center">登録日</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
-                        <tr class="hover:bg-gray-100">
-                            <td class="py-2 px-4 border-b text-left">{{ $user->id }}</td>
-                            <td class="py-2 px-4 border-b text-left"><a href="#">{{ $user->name }}</a>
-                            </td>
-                            <td class="py-2 px-4 border-b text-left">{{ $user->email }}</td>
-                            <td class="py-2 px-4 border-b text-left">
+        <div class="p-8">
+            <div class="w-3/5 m-auto mt-8">
+                <h1 class="text-2xl font-bold text-center mb-8">ユーザー一覧</h1>
+                <div class="overflow-x-auto mb-4">
+                    <table class="min-w-full border border-gray-600">
+                        <thead>
+                            <tr>
+                                <th class="py-2 px-4 bg-gray-200 border-b text-left">ID</th>
+                                <th class="py-2 px-4 bg-gray-200 border-b text-left">名前</th>
+                                <th class="py-2 px-4 bg-gray-200 border-b text-left">メールアドレス</th>
+                                <th class="py-2 px-4 bg-gray-200 border-b text-left">雇用状態</th>
+                                <th class="py-2 px-4 bg-gray-200 border-b text-center">登録日</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr class="hover:bg-gray-100">
+                                    <td class="py-2 px-4 border-b text-left">{{ $user->id }}</td>
+                                    <td class="py-2 px-4 border-b text-left"><a href="#">{{ $user->name }}</a>
+                                    </td>
+                                    <td class="py-2 px-4 border-b text-left">{{ $user->email }}</td>
+                                    <td class="py-2 px-4 border-b text-left">
                                         @if ($user->isEnrollment())
                                             在籍
                                         @endif
                                     </td>
-                            <td class="py-2 px-4 border-b text-center">{{ $user->created_at }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            {{ $users->links() }}
-        </x-reports.layout>
+                                    <td class="py-2 px-4 border-b text-center">{{ $user->created_at }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                {{ $users->links() }}
+            </div>
+        </div>
     </body>
 
     </html>
