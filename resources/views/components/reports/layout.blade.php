f8015c0eb095d38d35643afe24b3517c4f3b6308
tomoki <tmdesign.0223@gmail.com>
2024-01-24 23:28:00 +0900

    レイアウト用のコンポーネント

---
+++ resources/views/components/reports/layout.blade.php
@@ -0,0 +1,8 @@
+<div class="bg-gray-300 h-screen">
+    <div class="w-screen m-auto mt-8">
+        <h1 class="text-2xl font-bold text-center mb-8">{{ $title }}</h1>
+        <div class="w-4/5 mx-auto p-8 bg-white border rounded-md shadow-md">
+            {{ $slot }}
+        </div>
+    </div>
+</div>
