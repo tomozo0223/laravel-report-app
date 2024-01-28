75ca05209b03d01d22a070ca03143b5bf1a36ab0
tomoki <tmdesign.0223@gmail.com>
2024-01-28 22:34:12 +0900

    デザイン修正

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -1,50 +1,34 @@
 <x-app-layout>
-    <!DOCTYPE html>
-    <html lang="ja">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>日報一覧</title>
-    </head>
-
-    <body>
-        <div class="bg-gray-300 h-screen">
-            <div class="w-screen m-auto mt-8">
-                <h1 class="text-2xl font-bold text-center mb-8">日報一覧</h1>
-                <div class="w-4/5 mx-auto p-8 bg-white border rounded-md shadow-md">
-                    @if (session('message'))
-                        <p class="text-red-500 font-bold">{{ session('message') }}
-                        </p>
-                    @endif
-                    <table class="w-4/5 m-auto border border-gray-600">
-                        <thead>
-                            <tr>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-left">ID</th>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-left">日付</th>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-left">現場名</th>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-left">作業責任者</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($reports as $report)
-                                <tr class="hover:bg-gray-100">
-                                    <td class="py-2 px-4 border-b text-left">{{ $report->id }}</td>
-                                    <td class="py-2 px-4 border-b text-left"><a
-                                            href="{{ route('report.show', $report) }}"><strong>{{ $report->working_day }}</strong></a>
-                                    </td>
-                                    <td class="py-2 px-4 border-b text-left">{{ $report->site_name }}</td>
-                                    <td class="py-2 px-4 border-b text-left">{{ $report->user->name }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{ $reports->links() }}
-                </div>
-            </div>
-        </div>
-    </body>
-
-    </html>
+    <x-slot:header>
+        <h1 class="inline border-b-4 border-blue-300 pb-2">日報一覧</h1>
+    </x-slot:header>
+    <div class="w-4/5 m-auto">
+        @if (session('message'))
+            <p class="text-red-500 font-bold">{{ session('message') }}
+            </p>
+        @endif
+        <table class="w-full m-auto border border-gray-600 bg-white">
+            <thead>
+                <tr>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-left">ID</th>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-left">日付</th>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-left">現場名</th>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-left">作業責任者</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($reports as $report)
+                    <tr class="hover:bg-gray-100">
+                        <td class="py-2 px-4 border-b text-left">{{ $report->id }}</td>
+                        <td class="py-2 px-4 border-b text-left"><a
+                                href="{{ route('report.show', $report) }}"><strong>{{ $report->working_day }}</strong></a>
+                        </td>
+                        <td class="py-2 px-4 border-b text-left">{{ $report->site_name }}</td>
+                        <td class="py-2 px-4 border-b text-left">{{ $report->user->name }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    {{ $reports->links() }}
 </x-app-layout>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,51 +1,35 @@
 <x-app-layout>
-    <!DOCTYPE html>
-    <html lang="ja">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>ユーザー一覧ページ</title>
-    </head>
-
-    <body>
-        <div class="bg-gray-300 h-screen">
-            <div class="w-screen m-auto mt-8">
-                <h1 class="text-2xl font-bold text-center mb-8">ユーザー一覧</h1>
-                <div class="w-4/5 mx-auto p-8 bg-white border rounded-md shadow-md">
-                    <table class="w-4/5 m-auto border border-gray-600">
-                        <thead>
-                            <tr>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-left">ID</th>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-left">名前</th>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-left">メールアドレス</th>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-left">雇用状態</th>
-                                <th class="py-2 px-4 bg-gray-200 border-b text-center">登録日</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($users as $user)
-                                <tr class="hover:bg-gray-100">
-                                    <td class="py-2 px-4 border-b text-left">{{ $user->id }}</td>
-                                    <td class="py-2 px-4 border-b text-left"><a href="#">{{ $user->name }}</a>
-                                    </td>
-                                    <td class="py-2 px-4 border-b text-left">{{ $user->email }}</td>
-                                    <td class="py-2 px-4 border-b text-left">
-                                        @if ($user->isEnrollment())
-                                            在籍
-                                        @endif
-                                    </td>
-                                    <td class="py-2 px-4 border-b text-center">{{ $user->created_at }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                {{ $users->links() }}
-            </div>
-        </div>
-    </body>
-
-    </html>
+    <x-slot:header>
+        <h1 class="inline border-b-4 border-blue-300 pb-2">ユーザー一覧</h1>
+    </x-slot:header>
+    <div class="w-4/5 m-auto">
+        <table class="w-full m-auto border border-gray-600">
+            <thead>
+                <tr>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-left">ID</th>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-left">名前</th>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-left">メールアドレス</th>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-left">雇用状態</th>
+                    <th class="py-2 px-4 bg-blue-300 border-b text-center">登録日</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr class="hover:bg-gray-100">
+                        <td class="py-2 px-4 border-b text-left">{{ $user->id }}</td>
+                        <td class="py-2 px-4 border-b text-left"><a href="#">{{ $user->name }}</a>
+                        </td>
+                        <td class="py-2 px-4 border-b text-left">{{ $user->email }}</td>
+                        <td class="py-2 px-4 border-b text-left">
+                            @if ($user->isEnrollment())
+                                在籍
+                            @endif
+                        </td>
+                        <td class="py-2 px-4 border-b text-center">{{ $user->created_at }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    {{ $users->links() }}
 </x-app-layout>
