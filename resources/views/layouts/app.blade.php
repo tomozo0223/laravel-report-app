8d84d76c98f792c9b8003aceaaba4c4f13de4ab9
tomoki <tmdesign.0223@gmail.com>
2024-02-11 15:23:03 +0900

    ファビコンのパス修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="icon" href="{{ asset('storage/images/favicon.png') }}">
+    <link rel="icon" href="{{ asset('/images/favicon.png') }}">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.5.1/css/all.css">
 
