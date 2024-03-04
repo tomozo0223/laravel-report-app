b7d58e4d446220f1629506a38a8633129966d038
tomoki <tmdesign.0223@gmail.com>
2024-03-04 13:12:07 +0900

    テーブルの表示を修正

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -2,14 +2,14 @@
     <x-slot:header>
         <h1 class="inline border-b-4 border-blue-600 pb-2">日報一覧</h1>
     </x-slot:header>
-    <div class="max-w-screen-lg w-4/5 m-auto">
+    <div class="max-w-screen-lg md:w-4/5 w-96 m-auto">
         @if (session('message'))
             <p class="text-red-500 font-bold">{{ session('message') }}
             </p>
         @endif
         <table class="w-full m-auto border border-gray-600 bg-white">
             <thead>
-                <tr>
+                <tr class="text-sm md:text-base">
                     <th class="py-2 px-4 bg-blue-600 border-b text-white text-left">日付</th>
                     <th class="py-2 px-4 bg-blue-600 border-b text-white text-left">現場名</th>
                     <th class="py-2 px-4 bg-blue-600 border-b text-white text-left">作業責任者</th>
@@ -17,7 +17,7 @@
             </thead>
             <tbody>
                 @foreach ($reports as $index => $report)
-                    <tr class="hover:bg-gray-100 cursor-pointer {{ $index % 2 === 1 ? 'bg-blue-100' : '' }}"
+                    <tr class="text-sm md:text-base hover:bg-gray-100 cursor-pointer {{ $index % 2 === 1 ? 'bg-blue-100' : '' }}"
                         onclick="location.href='{{ route('report.show', $report) }}'">
                         <td class="py-2 px-4 border-b text-left font-bold">
                             {{ $report->working_day }}
