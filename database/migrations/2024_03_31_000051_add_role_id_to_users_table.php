f18f6fe71cb37ef3055a45acd16a2a74fdac2a92
tomoki <tmdesign.0223@gmail.com>
2024-04-01 20:45:45 +0900

    role_idをroleに修正

--- database/migrations/2024_03_31_000051_add_role_id_to_users_table.php
+++ database/migrations/2024_03_31_000051_add_role_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role_id')->after('employmentStatus');
+            $table->integer('role')->after('employmentStatus');
         });
     }
 
