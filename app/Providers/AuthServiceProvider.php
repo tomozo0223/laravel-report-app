ba27d2f1d3c8631a587dd2fc42189b5e59bbbb07
tomoki <tmdesign.0223@gmail.com>
2024-04-03 15:13:53 +0900

    ReportPolicyの呼び出し

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Report;
+use App\Policies\ReportPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Report::class => ReportPolicy::class,
     ];
 
     /**
