a16f7563428f77151ad7e1e6b3feac41a59657f2
tomoki <tmdesign.0223@gmail.com>
2024-04-16 20:45:38 +0900

    シーダー追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            SiteSeeder::class,
             ReportSeeder::class,
             CommentSeeder::class,
         ]);
