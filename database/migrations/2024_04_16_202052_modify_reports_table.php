50d725dd28a5ac18bc415573d8d218121b869769
tomoki <tmdesign.0223@gmail.com>
2024-04-16 20:30:54 +0900

    reportsテーブル修正ファイル追加

---
+++ database/migrations/2024_04_16_202052_modify_reports_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropColumn('site_name');
+            $table->foreignId('site_id')->after('id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropColumn('site_id');
+            $table->string('site_name')->after('id');
+        });
+    }
+};
