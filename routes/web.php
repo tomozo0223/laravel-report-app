4bd70dc9f9db1a0e69c3574800f2a1387701b04b
tomoki <tmdesign.0223@gmail.com>
2024-04-24 10:33:17 +0900

    scheduleルートの追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminUserController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReportController;
+use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -59,6 +60,12 @@ Route::middleware('auth')->group(function () {
             Route::get('/{user}', 'show')->name('show');
             Route::get('/{user}/edit', 'edit')->name('edit');
             Route::patch('/{user}', 'update')->name('update');
+        });
+    Route::prefix('schedule')
+        ->name('schedule.')
+        ->controller(ScheduleController::class)
+        ->group(function () {
+            Route::get('/', 'index')->name('index');
         });
 });
 
