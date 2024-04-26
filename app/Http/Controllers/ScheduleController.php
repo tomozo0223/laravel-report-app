0fec8f1a522db47df1c1b94b3a504482e412650a
tomoki <tmdesign.0223@gmail.com>
2024-04-26 15:32:15 +0900

    createメソッド追加

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Schedule;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -13,5 +14,11 @@ class ScheduleController extends Controller
         $schedules = Schedule::with(['site', 'users'])->paginate(10);
 
         return view('schedule.index', compact('schedules'));
+    }
+
+    public function create()
+    {
+        $users = User::all();
+        return view('schedule.create', compact('users'));
     }
 }
