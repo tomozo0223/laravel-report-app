1e0ce56243226574159a0501688dc77fe7e3690f
tomoki <tmdesign.0223@gmail.com>
2024-04-29 21:19:19 +0900

    予定詳細用のルート追加

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/', 'index')->name('index');
             Route::get('/create', 'create')->name('create');
             Route::post('/', 'store')->name('store');
+            Route::get('/{schedule}', 'show')->name('show');
         });
 });
 
