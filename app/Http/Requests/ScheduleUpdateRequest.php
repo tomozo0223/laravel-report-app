23a7279e7b09f04abc6457340a3ae57ada2d7cd8
tomoki <tmdesign.0223@gmail.com>
2024-04-30 13:59:43 +0900

    予定更新時のフォームリクエスト作成

---
+++ app/Http/Requests/ScheduleUpdateRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ScheduleUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'site_name' => ['required', 'string', 'max:20'],
+            'address' => ['required', 'string'],
+            'member_id' => ['required', 'array'],
+            'working_day' => ['required', 'date', 'after:today'],
+            'work_details' => ['required', 'string', 'max:100'],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'site_name' => '現場',
+            'address' => '住所',
+            'member_id' => 'メンバー',
+            'working_day' => '作業日',
+            'work_details' => '作業内容',
+        ];
+    }
+}
