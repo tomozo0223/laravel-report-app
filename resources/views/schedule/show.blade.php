f9ea17a5ccc7230da19e9685e5cc2174e99d7b73
tomoki <tmdesign.0223@gmail.com>
2024-04-30 13:57:04 +0900

    赤を黒に変更 更新を編集に修正

--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -3,17 +3,24 @@
         <h1 class="inline border-b-4 border-blue-300 pb-2">予定詳細</h1>
     </x-slot:header>
     <div class="max-w-screen-lg md:w-4/5 w-96 m-auto p-4 rounded">
+        @if (session('message'))
+            <p class="text-red-600">{{ session('message') }}</p>
+        @endif
         <div class="border p-4">
             <div class="border-b border-gray-300 mb-4">
-                <p class="text-red-400 font-bold">現場名</p>
+                <p class="text-black font-bold">現場名</p>
                 <h2 class="text-xl font-bold">{{ $schedule->site->name }}</h2>
             </div>
             <div class="border-b border-gray-300 mb-4">
-                <p class="text-red-400 font-bold">住所</p>
+                <p class="text-black font-bold">住所</p>
                 <p>{{ $schedule->site->address }}</p>
             </div>
             <div class="border-b border-gray-300 mb-4">
-                <p class="text-red-400 font-bold">メンバー</p>
+                <p class="text-black font-bold">作業予定日</p>
+                <p>{{ $schedule->working_day }}</p>
+            </div>
+            <div class="border-b border-gray-300 mb-4">
+                <p class="text-black font-bold">メンバー</p>
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
                     @foreach ($schedule->users as $user)
                         <p>{{ $user->name }}</p>
@@ -21,12 +28,12 @@
                 </div>
             </div>
             <div class="border-b border-gray-300 mb-4 drop-shadow-lg">
-                <p class="text-red-400 mb-2 font-bold">作業内容</p>
+                <p class="text-black mb-2 font-bold">作業内容</p>
                 <p class="border p-2">{!! nl2br($schedule->work_details) !!}</p>
             </div>
             <div class="flex justify-end">
                 <x-primary-button class="bg-green-600">
-                    <a href="{{ route('schedule.edit', $schedule) }}">更新</a>
+                    <a href="{{ route('schedule.edit', $schedule) }}">編集</a>
                 </x-primary-button>
                 <x-primary-button class="bg-red-600 ml-2">
                     削除
