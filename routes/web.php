c9ef4179ca706803a00b96cc385023f9656a3ae3
tomoki <tmdesign.0223@gmail.com>
2024-05-12 17:01:47 +0900

    createルート追加

--- routes/web.php
+++ routes/web.php
@@ -78,6 +78,7 @@ Route::middleware('auth')->group(function () {
         ->controller(SiteController::class)
         ->group(function () {
             Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
             Route::post('/', 'store')->name('store');
         });
 });
