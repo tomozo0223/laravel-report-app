3ee0001c68ed3e6c068606605bfad238b44261a5
tomoki <tmdesign.0223@gmail.com>
2024-05-12 17:02:27 +0900

    フォームをボタンに変更

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -3,31 +3,12 @@
         <h1 class="inline border-b-4 border-blue-300 pb-2">現場一覧</h1>
     </x-slot:header>
     <div class="max-w-screen-lg md:w-4/5 w-96 m-auto p-4 rounded">
+        <x-primary-button class="mb-4 bg-blue-500">
+            <a href="{{ route('site.create') }}">現場登録</a>
+        </x-primary-button>
         @if (session('message'))
             <p class="text-red-600">{{ session('message') }}</p>
         @endif
-        <form action="{{ route('site.store') }}" method="POST" class="mb-4">
-            @csrf
-            <div class="mb-2">
-                <label for="name" class="block text-sm font-semibold text-gray-600">現場名:</label>
-                <input type="text" name="name" id="name" class="w-full p-2 border rounded-md">
-                @error('name')
-                    <p class="text-red-600">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="mb-2">
-                <label for="address" class="block text-sm font-semibold text-gray-600">現場住所:</label>
-                <input type="text" name="address" id="address" class="w-full p-2 border rounded-md">
-                @error('address')
-                    <p class="text-red-600">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="text-right">
-                <x-primary-button class="bg-blue-500">
-                    登録
-                </x-primary-button>
-            </div>
-        </form>
         <table class="w-full m-auto border border-gray-600 bg-white">
             <thead>
                 <tr class="text-sm md:text-base">
